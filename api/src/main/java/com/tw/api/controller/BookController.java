6a3eae520841bdb732da0feab918434a965adc73
Rashdul Islam Ehasan <rashdul.ehasan@Rashduls-MacBook-Pro.local>
2020-03-29 12:15:35 +0100

    pre eject

---
+++ api/src/main/java/com/tw/api/controller/BookController.java
@@ -0,0 +1,9 @@
+package com.tw.api.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController {
+    public BookController() {
+    }
+}
---
+++ api/src/test/java/com/tw/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tw.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
